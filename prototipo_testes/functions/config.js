037bf3bd8bca217ad4f8c98b640f1738982edfda
Alexandre Lopes <alxlopes@live.com.pt>
2020-03-10 15:27:27 +0000

    updated demo prototype files due to migration to a separate Firebase project

---
+++ prototipo_testes/functions/config.js
@@ -0,0 +1,29 @@
+
+/*
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCigh_aAFdxO6Bn-_UfAVAhji976HTw5mE",
+  authDomain: "associacao-de-pais-prototipo.firebaseapp.com",
+  databaseURL: "https://associacao-de-pais-prototipo.firebaseio.com",
+  projectId: "associacao-de-pais-prototipo",
+  storageBucket: "associacao-de-pais-prototipo.appspot.com",
+  messagingSenderId: "689249889673",
+  appId: "1:689249889673:web:406aa9358b50e49e700558",
+  measurementId: "G-TH9RVBNE17"
+};
+
+if (!firebase.apps.length) {
+    // Initialize Firebase only for the first time
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+var firestore = firebase.firestore();
+//firebase.analytics();
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
+// Create a storage reference from our storage service
+var storageRef = storage.ref();
+*/
