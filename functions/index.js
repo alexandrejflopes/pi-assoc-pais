fd9417bfc560cb21ac72a5f6dfac33cc4a0c5e7f
Francisco Aires Fontinha <45005597+FFontinha@users.noreply.github.com>
2020-04-30 14:59:15 +0100

    index.js updated

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,8 @@
 
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
+admin.initializeApp();
 // Configure the email transport using the default SMTP transport and a GMail account.
 // For Gmail, enable these:
 // 1. https://www.google.com/settings/security/lesssecureapps
@@ -37,42 +39,6 @@ const mailTransport = nodemailer.createTransport({
 // TODO: Change this to your app or company name to customize the email sent.
 const APP_NAME = 'Aplicação teste';
 
-// [START sendWelcomeEmail]
-/**
- * Sends a welcome email to new user.
- */
-// [START onCreateTrigger]
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-// [END onCreateTrigger]
-  // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
-  // [END eventAttributes]
-  var actionCodeSettings = {
-  // URL you want to redirect back to. The domain (www.example.com) for this
-  // URL must be whitelisted in the Firebase Console.
-  url: 'http://testproject-269510.firebaseapp.com',
-  // This must be true.
-  handleCodeInApp: true,
-};
-  firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-  return sendWelcomeEmail(email, displayName);
-});
-// [END sendWelcomeEmail]
-
-// [START sendByeEmail]
-/**
- * Send an account deleted email confirmation to users who delete their accounts.
- */
-// [START onDeleteTrigger]
-exports.sendByeEmail = functions.auth.user().onDelete((user) => {
-// [END onDeleteTrigger]
-  const email = user.email;
-  const displayName = user.displayName;
-
-  return sendGoodbyeEmail(email, displayName);
-});
-// [END sendByeEmail]
 
 // Sends a welcome email to the given user.
 async function sendWelcomeEmail(email, displayName) {
@@ -103,3 +69,100 @@ async function sendGoodbyeEmail(email, displayName) {
   console.log('Account deletion confirmation email sent to:', email);
   return null;
 }
+
+
+exports.sendAuthenticationEmail = functions.https.onRequest((request, response) => {
+    let email = request.query.email;
+    let nome = request.query.nome;
+
+    var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://testproject-269510.firebaseapp.com/',
+    // This must be true.
+    handleCodeInApp: true,
+    };
+    //firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    admin.auth().generateSignInWithEmailLink(email, actionCodeSettings).then(link => {
+        let message = "Olá, "+nome+"\n\nRecebemos um pedido de autenticação para a sua conta na plataforma da sua associação de pais. Para entrar, por favor clique no link abaixo.\n\nLink: "+link+"\n\nSe considera que se trata de um erro, por favor ignore este email ou contacte os órgãos sociais da associação.\n\nAtenciosamente,\nA Equipa";
+
+        let subject = `Autenticação para associação de pais - ${APP_NAME}!`;
+
+        response.status(204).send();
+        return sendEmail(email, subject, message)
+    })
+    .catch(err => {
+        console.log('Error creating sign in email', err);
+        return response.status(405).send({"error" : err});
+    });
+});
+
+exports.sendNotificationEmail = functions.https.onRequest((request, response) => {
+    let email = request.query.email;
+    let nome = request.query.nome;
+   	let message = request.query.message;
+
+    var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://testproject-269510.firebaseapp.com/',
+    // This must be true.
+    handleCodeInApp: true,
+    };
+    //firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    admin.auth().generateSignInWithEmailLink(email, actionCodeSettings).then(link => {
+        let subject = `Autenticação para associação de pais - ${APP_NAME}!`;
+
+        response.status(204).send();
+        return sendEmail(email, subject, message)
+    })
+    .catch(err => {
+        console.log('Error creating sign in email', err);
+        return response.status(405).send({"error" : err});
+    });
+});
+
+exports.sendPositionEmail = functions.https.onRequest((request, response) => {
+    let email = request.query.email;
+    let nome = request.query.nome;
+    let nome2 = request.query.nome2;
+   	let cargo = request.query.cargo;
+
+    var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://testproject-269510.firebaseapp.com/',
+    // This must be true.
+    handleCodeInApp: true,
+    };
+    //firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    admin.auth().generateSignInWithEmailLink(email, actionCodeSettings).then(link => {
+        let message = "Olá, "+nome+"\n\nFoi feita um transição do cargo "+cargo+" do utilizador "+nome2+" para si. Para entrar, por favor clique no link abaixo.\n\nLink: "+link+"\n\nSe considera que se trata de um erro, por favor ignore este email ou contacte os órgãos sociais da associação.\n\nAtenciosamente,\nA Equipa";
+
+        let subject = `Autenticação para associação de pais - ${APP_NAME}!`;
+
+        response.status(204).send();
+        return sendEmail(email, subject, message)
+    })
+    .catch(err => {
+        console.log('Error creating sign in email', err);
+        return response.status(405).send({"error" : err});
+    });
+});
+
+/**
+ * Função auxiliar utilizada para enviar emails
+ */
+async function sendEmail(email, subject, message) {
+    const mailOptions = {
+      from: `${APP_NAME} <noreply@firebase.com>`,
+      to: email,
+    };
+  
+    // The user subscribed to the newsletter.
+    mailOptions.subject = subject;
+    mailOptions.text = message;
+    await mailTransport.sendMail(mailOptions);
+    //console.log('New welcome email sent to:', email);
+    return null;
+  }
