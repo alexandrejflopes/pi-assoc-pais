b0685cdf982bcd7845579fd5b1ba615ba037593b
Francisco Aires <f.fontinha@ua.pt>
2020-05-29 20:35:42 +0100

    Updates

---
+++ precache-manifest.5be0838c33ce0c0acfc58d97c9150ae3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490ca5e313fd77622517c9148ed7b902",
+    "url": "/pi-assoc-pais/index.html"
+  },
+  {
+    "revision": "4b166f3c9580bb11bfa7",
+    "url": "/pi-assoc-pais/static/js/2.972f4f99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pi-assoc-pais/static/js/2.972f4f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f28684ef5bca73e9e2",
+    "url": "/pi-assoc-pais/static/js/main.4466ef29.chunk.js"
+  },
+  {
+    "revision": "95a7e1aad30165b4801f",
+    "url": "/pi-assoc-pais/static/js/runtime-main.38f52bdf.js"
+  },
+  {
+    "revision": "8d04d9bac0f79735c4d960086e67bce1",
+    "url": "/pi-assoc-pais/static/media/assoc-pais-logo.8d04d9ba.png"
+  },
+  {
+    "revision": "9d1403c3ec0668184381d6490fa21a8d",
+    "url": "/pi-assoc-pais/static/media/cc.9d1403c3.png"
+  },
+  {
+    "revision": "b439a446e7a5688e1c05861abd139ea1",
+    "url": "/pi-assoc-pais/static/media/github_logo.b439a446.png"
+  },
+  {
+    "revision": "0271d2c6e35916cc9399994986eb5a64",
+    "url": "/pi-assoc-pais/static/media/logo_firebase.0271d2c6.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/pi-assoc-pais/static/media/logo_react.70f87bba.png"
+  }
+]);
