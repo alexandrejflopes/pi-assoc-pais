c41b0dd2c5462ee55abacd78f0638b21e5565526
alexandrejflopes <alxlopes@live.com.pt>
2020-06-26 14:33:26 +0100

    remove develop folder as all code is already in src folder in webapp folder

--- webapp/develop/src/flux/index.js
+++
@@ -1,6 +0,0 @@
-import Constants from "./constants";
-import Dispatcher from "./dispatcher";
-import Store from "./store";
-import StoreAdmin from "./storeAdmin";
-
-export { Constants, Dispatcher, Store, StoreAdmin };
--- webapp/develop/src/layouts/index.js
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from "./Default";
-import DefaultConfigLayout from "./DefaultConfigLayout";
-import DefaultAdminLayout from "./DefaultAdminLayout";
-
-export { DefaultLayout, DefaultConfigLayout, DefaultAdminLayout };
